2a8ce9c18ca01d2cfda9bec721e07674dd8076e2
Emre Ozbek <mehmet.ozbek@kcl.ac.uk>
2023-10-11 13:23:42 +0100

    Finished solution

--- primes.py
+++ primes.py
@@ -1,6 +1,25 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+
+    prime_list = []
+    i = 2
+    while len(prime_list) < number_of_primes:
+        if is_prime(i):
+            prime_list.append(i)
+        i += 1
+    return prime_list
